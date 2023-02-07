66bb212c9725f32b2ba60441134cf961327f6bc4
suhail th <suhailth17756@gmail.com>
2023-02-07 15:27:03 +0530

    form validation

--- auto.js
+++ auto.js
@@ -1,16 +1,36 @@
-$(document).ready(function(){
-    $("#signup").validate({
-        rules:{
-            fname:{
-                required:true 
-            }
-            
-        },
-        messages:{
-            fname:{
-                required:"enter first name",
-                minlength:"enter atleast 4 characters"
-            }
-        }
-    })
-})
+
+
+function validation(){
+    let name=document.getElementById("name").value.trim()
+    let email=document.getElementById("email").value.trim()
+    let place=document.getElementById("place").value.trim()
+    let message=document.getElementById("message").value.trim()
+    let phone=document.getElementById("phone").value.trim()
+    flag=0;
+    if(name==''){
+      document.getElementById("nameError").innerHTML="name is required"
+      flag=1;
+    }
+    if(email==''){
+      document.getElementById("emailError").innerHTML="email is required"
+      flag=1;
+    }
+    if(place==''){
+      document.getElementById("placeError").innerHTML="place is required"
+      flag=1;
+    }
+    if(phone==''){
+      document.getElementById("phoneError").innerHTML="message is required"
+      flag=1;
+    }
+    if(message==''){
+      document.getElementById("messageError").innerHTML="message is required"
+      falg=1;
+    }
+    if(flag==1){
+      return false;
+
+
+
+    }
+  }
